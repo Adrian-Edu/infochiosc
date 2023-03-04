9dbd6f7caefd67794aaab5c670c0e5c18e137137
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-03-04 19:36:57 +0200

    modificari imagini

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,38 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./Slide.css";
-import product from "../assets/career.png";
-import todo from "../assets/todo.png";
-import lucky from "../assets/lucky.png";
-import robo from "../assets/robo.png";
-import career from "../assets/career.png";
+import dedeman from "../assets/dedeman.png";
+import bosch from "../assets/bosch.png";
+import bioclinica from "../assets/bioclinica.png";
 
 function SlidePortfolio(props) {
   const currentPortfolio = [
     {
+      id: 0,
+      image: bosch,
+      url: "https://jobs.smartrecruiters.com/BoschGroup/743999827028321-your-start-bosch?trid=5ca12582-477f-422c-9efb-2f752450cb38",
+    },
+    {
       id: 1,
-      image: career,
-      url: "https://firme.peviitor.ro/",
+      image: dedeman,
+      url: "https://recrutare.dedeman.ro/detalii-post.php?id=82da4960-aadb-4b39-ac28-be14a1fee6a3",
     },
     {
       id: 2,
-      image: lucky,
-      url: "https://lucky-dice-ruby.vercel.app/",
-    },
-
-    {
-      id: 3,
-      image: todo,
-      url: "https://todo-app-six-lyart.vercel.app/",
-    },
-    {
-      id: 4,
-      image: product,
-      url: "https://lading-page-12.adrianedu.repl.co/",
-    },
-    {
-      id: 5,
-      image: robo,
-      url: "https://robo-joke-teller.adrianedu.repl.co/",
+      image: bioclinica,
+      url: "https://bioclinica.ro/compania/cariere/registrator-medical-bucuresti",
     },
   ];
 
